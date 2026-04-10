aaa591fdea0588315e03190b04aab1d34e695e5b
kunal-patel07 <kunnalpatel2004@gmail.com>
2026-04-10 15:12:17 +0530

    not able to access route without loigin

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import NavBar from './navBar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import NavBar from "./NavBar";
+import {    Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/Constant";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const userData = useSelector((store) => store.user);
+console.log(userData)
+
+  const fetchUser = async () => {
+
+    if(userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+     } catch (error) {
+      if(error.status === 400){
+        navigate("/login")
+      }
+        
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
-      <NavBar/>
-      <Outlet/>
-      <Footer/>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;

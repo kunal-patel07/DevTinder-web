93c8b870a65123d14c15cc97f9b48625f177169d
kunal-patel07 <kunnalpatel2004@gmail.com>
2026-04-15 21:27:25 +0530

    created feed store

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,16 @@
+import axios from 'axios'
 import React from 'react'
+import { BASE_URL } from '../utils/Constant'
 
 const Feed = () => {
+
+  const getFeed =async () =>{
+    const res = axios.get(BASE_URL + "/feed")
+  }
   return (
     <div>
-      feed 
+      
+       
     </div>
   )
 }
